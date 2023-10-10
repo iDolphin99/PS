d63135a71754239a93be55dab12c47a549a1d52d
iDolphin99 <idolphin.kr@gmail.com>
2023-10-10 23:52:26 +0900

    solving 1004

---
+++ Pytonic Basic/11279.py
@@ -0,0 +1,11 @@
+'''
+최대 힙
+첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 입력되는 자연수는 231보다 작다.
+'''
+from sys import stdin, stdout
+import heapq
+heapq = []
+input = stdin.readline
+for i in range(int(input())):
+    n = int(input())
+    print('시발왜않되') if n == 0 else print('시발돼')
