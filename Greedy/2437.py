4753357e8946bdc75faa6358ae2ef2b2975cd311
iDolphin99 <idolphin.kr@gmail.com>
2024-06-19 23:17:18 +0900

    0619 done

--- Greedy/2437.py
+++ Greedy/2437.py
@@ -5,19 +5,25 @@
 '''
 from sys import stdin 
 input = stdin.readline
-N = int(input())
-weights = sorted(list(map(int, input().rsplit())))
-print(weights)
+      
+def solution():
+    N = int(input())
+    weights = sorted(list(map(int, input().rsplit())))
 
-previous = [weights[0]]
-for i in range(1, N):
-    current = []
-    current.append(weights[i])
-    for p in previous:
-        if isinstance(p, int):
-            p = [p, weights[i]]
-        else:
-            p.append(weights[i])
-        current.append(p)
-    previous.append(current)
-print(previous)
+    possible, answer = [weights[0]], weights[0]+1
+    if answer != 2: 
+        print(1)
+    else:
+        for i in range(1, N):
+            possible.append(weights[i])
+            if weights[i] <= answer: 
+                possible.append(sum(weights[:i+1]))
+                answer = max(possible)+1
+            else:
+                break
+            # print("updated possible Ws: ", possible)
+            # print("updated answer: ", answer)  
+        print(answer)
+    
+if __name__ == "__main__":
+    solution()
