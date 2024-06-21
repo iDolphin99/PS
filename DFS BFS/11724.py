f6c8de3c203a501a74038bc5ed77d07f47e0d31c
iDolphin99 <idolphin.kr@gmail.com>
2024-06-21 18:50:17 +0900

    done 0621

--- DFS BFS/11724.py
+++ DFS BFS/11724.py
@@ -6,14 +6,29 @@
 from sys import stdin 
 from collections import deque
 input = stdin.readline
-N, M = map(int, input().rsplit())
-graph = [[] * (N) for _ in range(N)]
-for __ in range(M):
-    u, v = map(int, input().rsplit())
-    graph[u-1] += [v]
-    graph[v-1] += [u]
-print(graph)
-    
 
-  
-    
+def bfs():
+    answer = 0 
+    deq = deque([])
+    while vertex:
+        if len(deq) == 0:
+            answer += 1
+            V = vertex.pop()
+            deq = deque([V])
+        else:
+            current = deq.popleft()
+            for v in graph[current-1]:
+                if v not in deq and v in vertex:
+                    deq.append(v)
+                    vertex.remove(v)
+    print(answer)     
+            
+if __name__ == "__main__":
+    N, M = map(int, input().rsplit())
+    graph = [[] * (N) for _ in range(N)]
+    vertex = [i+1 for i in range(N)]
+    for __ in range(M):
+        u, v = map(int, input().rsplit())
+        graph[u-1] += [v]
+        graph[v-1] += [u] 
+    bfs()
