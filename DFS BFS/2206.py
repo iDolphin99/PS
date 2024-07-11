21a4666a0104d0aa8395a1bd4a252aa47ef43840
iDolphin99 <idolphin.kr@gmail.com>
2024-07-11 23:50:41 +0900

    done!!! 0711

---
+++ DFS BFS/2206.py
@@ -0,0 +1,45 @@
+'''
+벽 부수고 이동하기 
+첫째 줄: N(1 ≤ N ≤ 1,000), M(1 ≤ M ≤ 1,000)
+다음 N개의 줄에 M개의 숫자로 맵
+(1, 1)과 (N, M)은 항상 0이라고 가정
+'''
+from sys import stdin 
+from collections import deque 
+input = stdin.readline
+
+def BFS():
+    N, M = map(int, input().rsplit())
+    graph = [list(map(int, list(input().rstrip()))) for _ in range(N)]
+    visited = [[[0, 0] for _ in range(M)] for _ in range(N)] # 1: 안부수는, 0: 부순
+    visited[0][0][1] = 1
+    
+    directions = [(1,0), (0,1), (-1,0), (0,-1)]
+    deq = deque([(0,0,1)])
+    while deq:
+        x, y, break_cnt = deq.popleft()
+        if (x, y) == (N-1, M-1): 
+            return visited[x][y][break_cnt]
+        
+        for dx, dy in directions:
+            newX, newY = x+dx, y+dy
+            if 0 <= newX < N and 0 <= newY < M:
+                # 벽이 없다면?
+                if graph[newX][newY] == 0 and visited[newX][newY][1] == 0: 
+                    # 이전에 벽뚫 한 적이 없다면? 
+                    if break_cnt == 1 :
+                        visited[newX][newY][break_cnt] = visited[x][y][break_cnt] + 1
+                        deq.append((newX, newY, break_cnt))
+                    # 이전에 벽뚤한 적이 있다면?
+                    if break_cnt == 0 and visited[newX][newY][break_cnt] == 0:
+                        visited[newX][newY][break_cnt] = visited[x][y][break_cnt] + 1 
+                        deq.append((newX, newY, break_cnt))
+                    
+                # 벽이 있다면?
+                if graph[newX][newY] == 1 and break_cnt == 1 and visited[newX][newY][0] == 0: 
+                    visited[newX][newY][break_cnt-1] = visited[x][y][break_cnt] + 1
+                    deq.append((newX, newY, break_cnt-1))    
+    
+    return -1
+    
+print(BFS())
