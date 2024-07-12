cd0a3cbbb30aa11be376b399bd2c312fa0781912
iDolphin99 <idolphin.kr@gmail.com>
2024-07-12 23:34:08 +0900

    modified 0712

--- DFS BFS/2206.py
+++ DFS BFS/2206.py
@@ -8,10 +8,15 @@ from sys import stdin
 from collections import deque 
 input = stdin.readline
 
+def printgraph(g):
+    for i in range(len(g)): 
+        print(g[i])
+    print("----------------------------------")
+
 def BFS():
     N, M = map(int, input().rsplit())
     graph = [list(map(int, list(input().rstrip()))) for _ in range(N)]
-    visited = [[[0, 0] for _ in range(M)] for _ in range(N)] # 1: 안부수는, 0: 부순
+    visited = [[[0, 0] for _ in range(M)] for _ in range(N)] # 1: not broken, 0: broken
     visited[0][0][1] = 1
     
     directions = [(1,0), (0,1), (-1,0), (0,-1)]
@@ -20,26 +25,18 @@ def BFS():
         x, y, break_cnt = deq.popleft()
         if (x, y) == (N-1, M-1): 
             return visited[x][y][break_cnt]
-        
         for dx, dy in directions:
             newX, newY = x+dx, y+dy
             if 0 <= newX < N and 0 <= newY < M:
                 # 벽이 없다면?
-                if graph[newX][newY] == 0 and visited[newX][newY][1] == 0: 
-                    # 이전에 벽뚫 한 적이 없다면? 
-                    if break_cnt == 1 :
-                        visited[newX][newY][break_cnt] = visited[x][y][break_cnt] + 1
-                        deq.append((newX, newY, break_cnt))
-                    # 이전에 벽뚤한 적이 있다면?
-                    if break_cnt == 0 and visited[newX][newY][break_cnt] == 0:
-                        visited[newX][newY][break_cnt] = visited[x][y][break_cnt] + 1 
-                        deq.append((newX, newY, break_cnt))
-                    
+                if graph[newX][newY] == 0 and visited[newX][newY][break_cnt] == 0: 
+                    visited[newX][newY][break_cnt] = visited[x][y][break_cnt] + 1 
+                    deq.append((newX, newY, break_cnt))
                 # 벽이 있다면?
                 if graph[newX][newY] == 1 and break_cnt == 1 and visited[newX][newY][0] == 0: 
                     visited[newX][newY][break_cnt-1] = visited[x][y][break_cnt] + 1
                     deq.append((newX, newY, break_cnt-1))    
-    
+        # printgraph(visited) # for check
     return -1
     
 print(BFS())
