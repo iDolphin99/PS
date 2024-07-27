3545def27ad4933a2afee522e0696cc17bd4ca73
iDolphin99 <idolphin.kr@gmail.com>
2024-07-27 17:41:42 +0900

    done 0727

--- Prefix Sum/15565.py
+++ Prefix Sum/15565.py
@@ -3,17 +3,30 @@
 첫째 줄: N과 K가 주어진다. (1 ≤ K ≤ N ≤ 106)
 둘째 줄: N개의 인형 정보 (1 or 2)
 '''
+from collections import deque
 from sys import stdin
 
 def solution():
     input = stdin.readline
     N, K = map(int, input().rsplit())
-    toylist = list(map(int, input().rsplit()))
-    print(toylist)
+    toys = list(map(int, input().rsplit()))
+
+    answer = 1e6
+    ryan = deque([])
+    st, en, cnt = 0, 0, 0
+    while en < N:
+        if toys[en] == 1: 
+            ryan.append(en)
+            cnt += 1
+        if cnt == K:
+            st = ryan.popleft()
+            answer = min(answer, en-st+1)
+            if answer == K:
+                return K
+            cnt = K-1
+        en+=1
     
-    
-    
-    return -1
+    return answer if answer != 1e6 else -1
 
 if __name__ == "__main__":
     print(solution())
