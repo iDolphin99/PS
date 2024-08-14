45705bf6fbd2dc8a7ea59da6fb10c33c06ddf3d5
iDolphin99 <idolphin.kr@gmail.com>
2024-08-14 23:59:36 +0900

    solving... 0814

--- Dynamic Programming/10844.py
+++ Dynamic Programming/10844.py
@@ -1,12 +1,31 @@
 '''
 쉬운 계단수
+N은 1보다 크거나 같고, 100보다 작거나 같은 자연수
+첫째 줄에 정답을 1,000,000,000으로 나눈 나머지를 출력
 '''
 
 from sys import stdin
 
 def solution():
     N = int(stdin.readline())
-    return 1
+    dp = [[1 for _ in range(9)] for __ in range(N+1)]
+    print(dp)
+    
+    ans = 9
+    for i in range(2, N+1):
+        for j in range(10):
+            if j==0:
+                dp[i][j]=dp[i-1][1]
+            elif j==9:
+                dp[i][j]=dp[i-1][8]
+            else:
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1]
+        ans = sum(dp[i])
+    
+    print("changed: ")
+    print(dp)
+    
+    return ans
 
 if __name__ == "__main__":
     print(solution())
--- Dynamic Programming/2294.py
+++ Dynamic Programming/2294.py
@@ -17,7 +17,7 @@ def solution():
     dp[0] = 0
     for coin in coins:
         for i in range(coin, K+1):
-            if dp[i] > 0:
+            if dp[i] > 1:
                 dp[i] = min(dp[i], dp[i-coin]+1)
     return -1 if dp[K]==10001 else dp[K]
 
