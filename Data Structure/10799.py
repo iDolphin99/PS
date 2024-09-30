9961808aa791651f32b21c9cab96c1a70c17ecc1
iDolphin99 <idolphin.kr@gmail.com>
2024-09-30 23:58:06 +0900

    solving... 0930

---
+++ Data Structure/10799.py
@@ -0,0 +1,14 @@
+'''
+쇠막대기
+한 줄에 쇠막대기와 레이저의 배치를 나타내는 괄호 표현이 공백없이 주어짐
+- 괄호 문자의 개수는 최대 100,000
+'''
+
+from sys import stdin
+
+def solution():
+    
+    return 0
+
+if __name__ == "__main__":
+    print(solution())
