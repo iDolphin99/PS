36a67890f4618b534ba1243ad90ff2516436f515
iDolphin99 <idolphin.kr@gmail.com>
2024-10-08 23:56:57 +0900

    solving... 1008

--- Data Structure/2504.py
+++ Data Structure/2504.py
@@ -12,18 +12,27 @@ def solution():
     string = stdin.readline().rstrip()
     
     dic = {")": "(", "]": "["}
+    num = 1
+    isOpen = False
     stack = []
     answer = 0
     for s in string:
-        if s in dic.values():
-            stack.append(s)
-        elif s in dic.keys():
-            last = stack.pop()
-            if last != dic.get(s):
+        if s in dic.values(): # open
+            if s == "(": num *= 2 
+            else: num *= 3 
+            stack.append([s, num])
+            isOpen = True
+        elif s in dic.keys(): # close
+            if len(stack)==0 or stack[-1][0] != dic.get(s): 
                 break 
-            else:
-                answer += 1
-    
+            last = stack.pop()
+            if isOpen:
+                answer += last[1]
+            num = stack[-1][1]
+            isOpen = False
+        print(stack, answer)
+            
+            
     return answer
 
 if __name__ == "__main__":
